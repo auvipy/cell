f254519fa7e2119c141dac8a89c8e3946ff9c41f
Ask Solem <ask@celeryproject.org>
2011-08-18 17:40:07 +0100

    Fixes huge connection leak in cl.pools

--- cl/pools.py
+++ cl/pools.py
@@ -7,18 +7,33 @@ from itertools import chain
 from kombu.pools import ProducerPool
 
 __all__ = ["connections", "producers", "set_limit", "reset"]
-_limit = [100]
+_limit = [200]
 
 
-class _Connections(dict):
+class HashingDict(dict):
+
+    def __getitem__(self, key):
+        h = hash(key)
+        if h not in self:
+            return self.__missing__(key)
+        return dict.__getitem__(self, h)
+
+    def __setitem__(self, key, value):
+        return dict.__setitem__(self, hash(key), value)
+
+    def __delitem__(self, key):
+        return dict.__delitem__(self, hash(key))
+
+
+class _Connections(HashingDict):
 
     def __missing__(self, connection):
-        k = self[connection] = connection.Pool(_limit[0])
+        k = self[connection] = connection.Pool(limit=_limit[0])
         return k
 connections = _Connections()
 
 
-class _Producers(dict):
+class _Producers(HashingDict):
 
     def __missing__(self, conn):
         k = self[conn] = ProducerPool(connections[conn], limit=_limit[0])
