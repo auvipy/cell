716bb29d9494799184f4948518bc27b3f9ea7bbe
Ask Solem <ask@celeryproject.org>
2011-09-05 12:44:46 +0100

    Forgot to add cl.g

---
+++ cl/g/__init__.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+
+from kombu.syn import detect_environment
+
+from ..utils import cached_property
+
+G_NOT_FOUND = """\
+cl does not currently support %r, please use one of %s\
+"""
+
+
+class G(object):
+    map = {"eventlet": "_eventlet"}
+
+    def spawn(self, fun, *args, **kwargs):
+        return self.current.spawn(fun, *args, **kwargs)
+
+    def timer(self, interval, fun, *args, **kwargs):
+        return self.current.timer(interval, fun, *args, **kwargs)
+
+    def blocking(self, fun, *args, **kwargs):
+        return self.current.blocking(fun, *args, **kwargs)
+
+    def Queue(self, *args, **kwargs):
+        return self.current.Queue(*args, **kwargs)
+
+    def Event(self, *args, **kwargs):
+        return self.current.Event(*args, **kwargs)
+
+    @cached_property
+    def _eventlet(self):
+        from . import eventlet
+        return eventlet
+
+    @cached_property
+    def current(self):
+        type = detect_environment()
+        try:
+            return getattr(self, self.map[type])
+        except KeyError:
+            raise KeyError(G_NOT_FOUND % (type,
+                                          ", ".join(self.map.keys())))
+
+g = G()
+blocking = g.blocking
+spawn = g.spawn
+timer = g.timer
+Queue = g.Queue
+Event = g.Event
---
+++ cl/g/eventlet.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+
+import sys
+import warnings
+
+from eventlet import Timeout      # noqa
+from eventlet import event
+from eventlet import greenthread
+from eventlet import queue
+from greenlet import GreenletExit
+from kombu import syn
+
+blocking = syn.blocking
+spawn = greenthread.spawn
+Queue = queue.LightQueue
+Event = event.Event
+
+
+class Entry(object):
+    g = None
+
+    def __init__(self, interval, fun, *args, **kwargs):
+        self.interval = interval
+        self.fun = fun
+        self.args = args
+        self.kwargs = kwargs
+        self.cancelled = False
+        self._spawn()
+
+    def _spawn(self):
+        self.g = greenthread.spawn_after_local(self.interval, self)
+        self.g.link(self._exit)
+
+    def __call__(self):
+        try:
+            return blocking(self.fun, *self.args, **self.kwargs)
+        except Exception, exc:
+            warnings.warn("Periodic timer %r raised: %r" % (fun, exc),
+                            exc_info=sys.exc_info())
+        finally:
+            self._spawn()
+
+    def _exit(self, g):
+        try:
+            self.g.wait()
+        except GreenletExit:
+            self.cancel()
+
+    def cancel(self):
+        if self.g and not self.cancelled:
+            self.g.cancel()
+            self.cancelled = True
+
+    def kill(self):
+        if self.g:
+            try:
+                self.g.kill()
+            except GreenletExit:
+                pass
+
+    def __repr__(self):
+        return "<Entry: %r (%s)>" % (
+                    self.fun, "cancelled" if self.cancelled else "alive")
+
+
+def timer(interval, fun, *args, **kwargs):
+    return Entry(interval, fun, *args, **kwargs)
