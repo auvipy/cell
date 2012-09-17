75eacb13930ef03c1ebc1ff619f47b54cde85532
Rumyana Neykova <rumi.neykova@gmail.com>
2012-09-17 11:49:54 +0100

    Use the Server class (an Actor derived class)

--- examples/hello.py
+++ examples/hello.py
@@ -11,41 +11,17 @@ class GreetingActor(Server):
     default_routing_key = 'GreetingActor'
 
     class state:
-
         def greet(self, who='world'):
             return 'Hello %s' % who
 greeting = GreetingActor(connection)
+ 
 
-
-class Printer(Actor):
-    default_routing_key = 'Printer'
-           
-    class state:
-        def echo(self, msg = 'test'):
-            print 'I am a printer:',msg
-            #self.output_edge.send(msg)
-            return msg 
-        
-printerActor = Printer(connection)
-
-
-
-class Ihu(Actor):
-    default_routing_key = 'Printer'
-    
-    class state:
-        def temp(self, msg = 'blabla'):
-            self.output_server.send(msg)
-    
 
 class GreetingAgent(Agent):
-    actors = [greeting, printerActor]
+    actors = [greeting]
     
 if __name__ == '__main__':        
-        consumer = GreetingAgent(connection).consume_from_commandline()
-        for _ in consumer:
-            print 'Received'
-
+        GreetingAgent(connection).consume_from_commandline()
 # Run this script from the command line and try this
 # in another console:
 #
