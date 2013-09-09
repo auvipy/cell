0a119d53c6eaedb9e003b1f183be7c6a4ec12ad9
Rumyana Neykova <rumi.neykova@gmail.com>
2013-09-09 16:06:11 +0100

    Add comparison with cell

--- examples/hello.py
+++ examples/hello.py
@@ -1,11 +1,18 @@
-from cell.actors import Actor
+from kombu.common import uuid
+from cell.actors import Actor, ActorProxy
+from cell.agents import dAgent
+from kombu.connection import Connection
 
 
 class GreetingActor(Actor):
-
-    class state:
+    class state(Actor.state):
         def greet(self, who='world'):
-            print 'Hello %s' % who
+            return 'Hello %s' % who
+
+class ByeActor(Actor):
+    class state(GreetingActor.state):
+        def bye(self, who='world'):
+            print 'Bye %s' % who
 
 #Run from the command line:
 """
@@ -21,3 +28,13 @@ greeter = agent.select(GreetingActor)
 from examples.workflow import Printer
 id = agent.select(Printer)
 """
+
+
+if __name__=='__main__':
+    """agent = dAgent(Connection())
+    actor = agent.spawn(GreetingActor)
+
+    actor.send.greet({'who':'hello'})
+    actor.send.greet({'who':'hello'})
+    first_reply(actor.scatter.greet({'who':'hello'}))
+    """
